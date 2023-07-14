85e5dc108ad77dc5237030c4f6d0f4ea9abc29ef
Antonio Solida <solida.antonio@gmail.com>
2023-07-14 16:04:46 +0200

    Ping pong (seems working)

--- pub_sub/publisher.c
+++ pub_sub/publisher.c
@@ -1,15 +1,114 @@
-#include <czmq.h>
+#include "zhelpers.h"
+#include <unistd.h>
+
+int main (void){
+    int count = 0;
+
+    //  Prepare our context and publisher
+    void *context = zmq_ctx_new ();
+    void *publisher = zmq_socket (context, ZMQ_PUB);
+    zmq_bind (publisher, "tcp://*:5563");
+
+    // Prepare Pong context ad publisher
+    void *context_pong = zmq_ctx_new ();
+    void *subscriber_pong = zmq_socket (context_pong, ZMQ_SUB);
+    zmq_connect (subscriber_pong, "tcp://127.0.0.1:5564");
+    zmq_setsockopt (subscriber_pong, ZMQ_SUBSCRIBE, "Pong", 1);
+
+    /*
+    Basically, it takes a little time (a few milliseconds) for the connection to be set up, 
+    and in that time lots of messages can be lost. 
+    The publisher needs to sleep a little before starting to publish, 
+    or (better) it needs to explicitly synchronize with the subscriber (maybe soon - lol).
+    */
+    s_sleep(3000); //IMPORTANT!!!
+
+    while (1) {
+
+        char* message = (char*)malloc((count + 1) * sizeof(char)); // +1 for the null terminator
+        for(int i=0; i<count; i++){
+            message[i] = '0';
+        }
+        count++;
+
+        s_sendmore (publisher, "PING"); //envelope
+        s_send (publisher, message); //content
+        clock_t start_time = clock();
+        printf ("Pingo\n");
+        free(message);
+
+        //  Read envelope with address
+        char *address = s_recv (subscriber_pong);
+        //  Read message contents
+        char *contents = s_recv (subscriber_pong);
+        clock_t end_time = clock();
+        printf ("[%s] %s. RTT: %2f\n", address, contents, (double)(end_time - start_time) / CLOCKS_PER_SEC * 1000);
+        free (address);
+        free (contents);
+        s_sleep (250);
+    }
+    //  We never get here, but clean up anyhow
+    zmq_close (publisher);
+    zmq_ctx_destroy (context);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <czmq.h>
 
 int main(int argc, char ** argv) {
     zsock_t *socket = zsock_new_pub("tcp://127.0.0.1:5555"); //localhost non funge
     assert(socket);
+    zsock_t *socket2 = zsock_new_sub("tcp://127.0.0.1:5556", "TOPIC2");
+    assert(socket2);
+
+    zsock_send(socket, "ss", "TOPIC", "Init");
+
+    char *topic;
+    char *frame;
+    zmsg_t *msg;
 
     while(!zsys_interrupted) {
         zsys_info("Publishing");
-        zsock_send(socket, "sss", "TOPIC", "MESSAGE PART", "ANOTHER");//invia un messaggio con 3 stringhe
-        zclock_sleep(1000);
+        zsock_send(socket, "ss", "TOPIC", "ME");//invia un messaggio con nome topic + stringa
+        zclock_sleep(3000);
+
+        int rc = zsock_recv(socket2, "sm", &topic, &msg);
+        assert(rc == 0);
+        zsys_info("Recv on %s", topic);
+        while(frame = zmsg_popstr(msg)) { //poppa le stringhe dal mess.
+            zsys_info("> %s", frame);
+            free(frame);
+        }
     }
 
     zsock_destroy(&socket);
     return 0;
-}
+}*/
--- pub_sub/subscriber.c
+++ pub_sub/subscriber.c
@@ -1,4 +1,50 @@
-#include <czmq.h>
+#include "zhelpers.h"
+
+int main (void){
+    //  Prepare our context and subscriber
+    void *context = zmq_ctx_new ();
+    void *subscriber = zmq_socket (context, ZMQ_SUB);
+    zmq_connect (subscriber, "tcp://localhost:5563");
+    zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "PING", 1);
+
+    void *context_pong = zmq_ctx_new ();
+    void *publisher_pong = zmq_socket (context_pong, ZMQ_PUB);
+    zmq_bind (publisher_pong, "tcp://*:5564");
+
+    /*
+    Basically, it takes a little time (a few milliseconds) for the connection to be set up, 
+    and in that time lots of messages can be lost. 
+    The publisher needs to sleep a little before starting to publish, 
+    or (better) it needs to explicitly synchronize with the subscriber (maybe soon - lol).
+    */
+    s_sleep(3000);
+    
+    while (1) {
+        //  Read envelope with address
+        char *address = s_recv (subscriber);
+        //  Read message contents
+        char *contents = s_recv (subscriber);
+        printf ("[%s] %s\n", address, contents);
+        //printf ("[%s] \n", address);
+        free (address);
+        free (contents);
+        s_sendmore (publisher_pong, "Pong");
+        s_send (publisher_pong, " pong");
+    }
+    //  We never get here, but clean up anyhow
+    zmq_close (subscriber);
+    zmq_ctx_destroy (context);
+    return 0;
+}
+
+
+
+
+
+
+
+
+/*#include <czmq.h>
 
 int main(int argc, char ** argv) {
     zsock_t *socket = zsock_new_sub("tcp://127.0.0.1:5555", "TOPIC");
@@ -10,21 +56,22 @@ int main(int argc, char ** argv) {
     char *frame;
     zmsg_t *msg;
     
-
     while(!zsys_interrupted) {
         //zsys_info("Recv on %s", topic);
         int rc = zsock_recv(socket, "sm", &topic, &msg);//riceve uno string message
                                                         //questa funzione è bloccante, ovvero
                                                         //rimane in attesa di un messaggio
         assert(rc == 0);
+        zsys_info("Recv on %s", topic);
         while(frame = zmsg_popstr(msg)) { //poppa le stringhe dal mess.
             zsys_info("> %s", frame);
             free(frame);
         }
+        zsock_send(socket2, "ss", "TOPIC2", "MESSAGE PART");
     }
     free(topic);
     zmsg_destroy(&msg);
 
     zsock_destroy(&socket);
     return 0;
-}
+}*/
